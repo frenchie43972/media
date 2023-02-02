7c0935d686044ec5178e575a43b034c7fbbc29ae
French <1078924850117002@mil>
2023-02-02 14:16:23 +0900

    no real changes

--- media/src/index.js
+++ media/src/index.js
@@ -5,8 +5,8 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import App from './App';
 
-const el = document.getElementById('root');
-const root = createRoot(el);
+const root = createRoot(document.getElementById('root'));
+
 root.render(
     <Provider store={store}>
         <App />
